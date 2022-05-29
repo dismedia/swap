0d7739db7daf67ebb1ecdb73add123831f5c5af9
dismedia <dismed14@gmail.com>
2022-05-29 16:38:02 +0200

    fixed slighter:
    Swap.deposit(IERC20,uint256) (contracts/Swap.sol#30-35) ignores return value by sourceTokenAddress.transferFrom(msg.sender,address(this),amount) (contracts/Swap.sol#33)
    Swap.exchange(IERC20,uint256) (contracts/Swap.sol#38-58) ignores return value by _tokenA.transferFrom(msg.sender,address(this),amount) (contracts/Swap.sol#46)
    Swap.exchange(IERC20,uint256) (contracts/Swap.sol#38-58) ignores return value by _tokenB.transfer(msg.sender,10 ** 18 * amount / _rateAB) (contracts/Swap.sol#47)
    Swap.exchange(IERC20,uint256) (contracts/Swap.sol#38-58) ignores return value by _tokenB.transferFrom(msg.sender,address(this),amount) (contracts/Swap.sol#52)
    Swap.exchange(IERC20,uint256) (contracts/Swap.sol#38-58) ignores return value by _tokenA.transfer(msg.sender,amount * _rateAB / 10 ** 18) (contracts/Swap.sol#53)
    Swap.withdraw(IERC20,uint256) (contracts/Swap.sol#60-65) ignores return value by sourceTokenAddress.transfer(owner(),amount) (contracts/Swap.sol#63)
    Reference: https://github.com/crytic/slither/wiki/Detector-Documentation#unchecked-transfer

--- contracts/Swap.sol
+++ contracts/Swap.sol
@@ -2,15 +2,15 @@ pragma solidity ^0.8.0;
 
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "hardhat/console.sol";
-
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract Swap is Ownable {
 
     uint256 private _rateAB;
     IERC20 public _tokenA;
     IERC20 public _tokenB;
+
+    using SafeERC20 for IERC20;
 
     constructor(IERC20 tokenA, IERC20 tokenB, uint256 rateAB){
 
@@ -31,38 +31,35 @@ contract Swap is Ownable {
     function deposit(IERC20 sourceTokenAddress, uint256 amount) public onlyOwner returns (bool){
 
         require(sourceTokenAddress == _tokenA || sourceTokenAddress == _tokenB, "cannot deposit, unknown token address");
-        sourceTokenAddress.transferFrom(msg.sender, address(this), amount);
+
+        sourceTokenAddress.safeTransferFrom(msg.sender, address(this), amount);
         return true;
     }
 
 
     function exchange(IERC20 sourceTokenAddress, uint256 amount) public returns (bool){
 
-
         require(sourceTokenAddress == _tokenA || sourceTokenAddress == _tokenB, "cannot swap, unknown token address");
-
-
         if (sourceTokenAddress == _tokenA) {
 
-
-            uint256 b=10 ** 18 * amount / _rateAB;
+            uint256 b = 10 ** 18 * amount / _rateAB;
 
             require(b <= _tokenB.balanceOf(address(this)), "not enough token deposited");
             require(amount <= _tokenA.allowance(msg.sender, address(this)), "not enough token user allowance");
 
-            _tokenA.transferFrom(msg.sender, address(this), amount);
-            _tokenB.transfer(msg.sender, b);
+            _tokenA.safeTransferFrom(msg.sender, address(this), amount);
+            _tokenB.safeTransfer(msg.sender, b);
 
 
         } else {
 
-            uint256 a=amount * _rateAB / 10 ** 18;
+            uint256 a = amount * _rateAB / 10 ** 18;
 
-            require( a <= _tokenA.balanceOf(address(this)), "not enough token deposited");
+            require(a <= _tokenA.balanceOf(address(this)), "not enough token deposited");
             require(amount <= _tokenB.allowance(msg.sender, address(this)), "not enough token user allowance");
 
-            _tokenB.transferFrom(msg.sender, address(this), amount);
-            _tokenA.transfer(msg.sender, a);
+            _tokenB.safeTransferFrom(msg.sender, address(this), amount);
+            _tokenA.safeTransfer(msg.sender, a);
 
         }
 
@@ -72,7 +69,7 @@ contract Swap is Ownable {
     function withdraw(IERC20 sourceTokenAddress, uint256 amount) public onlyOwner returns (bool){
 
         require(sourceTokenAddress == _tokenA || sourceTokenAddress == _tokenB, "cannot withdraw, unknown token address");
-        sourceTokenAddress.transfer(owner(), amount);
+        sourceTokenAddress.safeTransfer(owner(), amount);
         return true;
     }
 }
